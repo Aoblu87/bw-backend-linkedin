d5d8846ef8a9978e8a5a5ff3d01df4e4672e204c
Stefania <stefania.struzzi@gmail.com>
2023-12-18 20:58:22 +0100

    Fatto partire server

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+// import list from "express-list-endpoints"
+
+const server = express();
+
+const port = 3025;
+
+server.get("/health", function (req, res) {
+  res.status(200).send();
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    server.listen(port, () => {
+      console.log("Server listening to port: " + port);
+      // console.table(list(server))
+    });
+  })
+  .catch(() => {
+    console.log("Errore nella connessione al DB");
+  });
