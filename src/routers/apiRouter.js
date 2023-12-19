c63450851897ba83adaf40650ad287f6e0e18266
Stefania <stefania.struzzi@gmail.com>
2023-12-19 10:49:49 +0100

    cambiato percorso profile

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import profileRouter from "./Profile/profileRouter.js";
+import profileRouter from "./profile/profileRouter.js";
 
 const apiRouter = express.Router();
 apiRouter.use(express.json());
