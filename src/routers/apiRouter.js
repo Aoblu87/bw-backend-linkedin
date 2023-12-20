94f4fb1c960b652904210c53f6bbfd23e670a29f
Stefania <stefania.struzzi@gmail.com>
2023-12-20 21:02:04 +0100

    ok

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import profilesRouter from "./profiles/profilesRouter.js";
 import cors from "cors";
-import experiencesRouter from "./experiences/experiencesRouter.js";
 
 const apiRouter = express.Router();
 apiRouter.use(express.json());
@@ -10,6 +9,5 @@ apiRouter.use(cors());
 
 //Rotta /profile
 apiRouter.use("/profiles", profilesRouter);
-apiRouter.use("/experiences", experiencesRouter);
 
 export default apiRouter;
