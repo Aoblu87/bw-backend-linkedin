28a002f970dee4c4b2e717e720864f33d87bb312
SERENABACCARI <serena88_2011@libero.it>
2023-12-20 17:25:34 +0100

    modigiche

--- src/routers/profiles/experiencesRouter.js
+++ src/routers/profiles/experiencesRouter.js
@@ -1,4 +1,25 @@
-import bcrypt from "bcrypt";
 import express from "express";
-import checkJwt from "../../middlewares/checkJwt.js";
-import cloudinaryUploader from "../../middlewares/uploadFile.js";
+
+const experienceRouter = express.Router();
+
+/* GET - ritorna l'esperienze di tutti gli utenti */
+experienceRouter.get('/:userid', async (req, res) => {
+    try {
+        const authors = await experience.findByUserId(req.params.userid);
+        res.json(authors);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+/* GET - ritorna le esperienze dell'utente loggato */
+experienceRouter.get('/loggato', async (req, res) => {
+    try {
+        
+        res.json({ message: 'utente loggato.' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+export default experienceRouter;
