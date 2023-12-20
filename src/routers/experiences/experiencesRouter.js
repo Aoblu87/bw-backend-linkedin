770ecfce0d459143549e5e883c0ab836e7023673
SERENABACCARI <serena88_2011@libero.it>
2023-12-20 19:29:49 +0100

    eliminato file  experiences

--- src/routers/experiences/experiencesRouter.js
+++ src/routers/experiences/experiencesRouter.js
@@ -3,6 +3,27 @@ import { Experience } from "../../models/experiences.js";
 
 const experiencesRouter = express.Router();
 
+/* GET - ritorna l'esperienze di tutti gli utenti */
+experienceRouter.get('/:userid', async (req, res) => {
+  try {
+    const experience = await experience.findByUserId(req.params.userid);
+    res.json(experience);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+/* GET - ritorna le esperienze dell'utente loggato */
+experienceRouter.get('/loggato', async (req, res) => {
+  try {
+
+    res.json({ message: 'utente loggato.' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 //POST - aggiunge una nuova esperienza
 experiencesRouter.post("/", async (req, res, next) => {
   try {
