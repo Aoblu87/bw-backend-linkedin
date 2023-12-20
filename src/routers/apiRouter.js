459bd8b140964731dfbabb1596df1d4eb3e85a8e
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-12-20 20:16:09 +0100

    up

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,6 @@
 import express from "express";
 import profilesRouter from "./profiles/profilesRouter.js";
-import experiencesRouter from "./experiences/experiencesRouter.js";
 import cors from "cors";
-import experiencesRouter from "./experiences/experiencesRouter.js";
 
 const apiRouter = express.Router();
 apiRouter.use(express.json());
@@ -11,6 +9,5 @@ apiRouter.use(cors());
 
 //Rotta /profile
 apiRouter.use("/profiles", profilesRouter);
-apiRouter.use("/experiences", experiencesRouter);
 
 export default apiRouter;
